ee96022291c7a41b0e5b4dc9c7a45d463908fbd2
Ricardo Rojo <ricardorojo@netscape.net>
2015-02-28 14:45:56 -0500

    bugs corrected

--- rock.rb
+++ rock.rb
@@ -17,26 +17,26 @@ def instructions
   puts "\n Tip: Spock is the best"
 end
 
-def total_results(games_played,games_won,games_lost)
+def total_results(games_played)
   puts "\n Number of games played #{games_played}".center(50)
-  puts "\n Number of games won #{games_won}"
-  puts "\n Number of games lost #{games_lost}"
+  puts "\n Number of games won #{@games_won}"
+  puts "\n Number of games lost #{@games_lost}"
 end
 
 def who_wins(player_play)
   computer_play = rand(1..5)
-  puts "computer choice #{choices[computer_play]}"
-  game_result = (choices.key(player_play) - computer_play) % 5
+  puts "computer choice #{@choices[computer_play]}"
+  game_result = (@choices.key(player_play) - computer_play) % 5
   puts "game result is #{game_result}"
   if (game_result.odd?)
-    puts "#{player_play} #{results[player_play][game_result]} #{choices[computer_play]}." + " You WIN!!"
-    games_won += 1
+    puts "#{player_play} #{@results[player_play][game_result]} #{@choices[computer_play]}." + " You WIN!!"
+    @games_won += 1
   elsif game_result == 0
     puts "It´s a draw"
   else
-    game_result = (computer_play - choices.key(player_play) ) % 5
-    games_lost += 1
-    puts "#{choices[computer_play]} #{results[choices[computer_play]][game_result]} #{player_play}." + " You loose!!"
+    game_result = (computer_play - @choices.key(player_play) ) % 5
+    @games_lost += 1
+    puts "#{@choices[computer_play]} #{@results[@choices[computer_play]][game_result]} #{player_play}." + " You loose!!"
   end
 end
 
@@ -44,12 +44,12 @@ instructions # show instructions
 
 # counters
 games_played = 0 
-games_won = 0
-games_lost = 0
+@games_won = 0
+@games_lost = 0
 
 # values hashes
-choices = {1 => "rock", 2 => "paper", 3 => "scissors", 4 => "spock", 5 => "lizard"}
-results = {"rock" => {1 => "crushes ", 3 => "crushes "}, "paper" => {1 => "covers ", 3 => "disproves "}, "scissors" => {1 => "cuts ", 3 => "decapitates"}, "spock" => {1 => "smashes ", 3 => "vaporizes "}, "lizard" => {1 => "poisons ", 3 => "eats "}}
+@choices = {1 => "rock", 2 => "paper", 3 => "scissors", 4 => "spock", 5 => "lizard"}
+@results = {"rock" => {1 => "crushes ", 3 => "crushes "}, "paper" => {1 => "covers ", 3 => "disproves "}, "scissors" => {1 => "cuts ", 3 => "decapitates"}, "spock" => {1 => "smashes ", 3 => "vaporizes "}, "lizard" => {1 => "poisons ", 3 => "eats "}}
 error_message = ["upps, wrong option.", "Option is wrong, try again", "That option is not logic, choose again"]
 
 # Action
@@ -60,7 +60,7 @@ begin
   
   if player_play == "exit"
     break
-  elsif results[player_play].nil? 
+  elsif @results[player_play].nil? 
     puts error_message.sample
   else
     who_wins(player_play)
@@ -68,4 +68,4 @@ begin
 end while player_play != "exit" 
 
 # results
-total_results(games_played,games_won,games_lost)
+total_results(games_played)
