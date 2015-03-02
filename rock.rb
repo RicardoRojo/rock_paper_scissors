888e9ac9cf0ff9300edc602e351b4b0aba488fd4
Ricardo Rojo <ricardorojo@netscape.net>
2015-03-02 18:55:21 -0500

    version 1.0

--- rock.rb
+++ rock.rb
@@ -5,67 +5,65 @@
 # RIP Leonard Nimoy. Thanks for all the good moments
 # ============
 # Operations taken from http://en.wikipedia.org/wiki/Rock-paper-scissors
-def instructions
-  puts "rock-paper-scissors-lizard-Spock".center(50)
-  50.times {print "="}
+def print_instructions
+  puts " Play rock-paper-scissors-lizard-Spock !".center(50)
   puts "\nROCK crashes scissors and lizard"
   puts "PAPER covers rock and disproves Spock"
   puts "SCISSORS cuts paper and decapitates lizard"
   puts "LIZARD eats paper and poisons Spock"
   puts "SPOCK vaporizes rock and smashes scissors"
-  50.times {print "="}
-  puts "\n Tip: Spock is the best"
+  puts "=========================================="
 end
 
-def total_results(games_played)
-  puts "\n Number of games played #{games_played}".center(50)
-  puts "\n Number of games won #{@games_won}"
-  puts "\n Number of games lost #{@games_lost}"
+def total_results(game_stats)
+  puts "Game Stats".center(50)
+  puts "Number of games played #{game_stats['games_played']}"
+  puts "Number of games won #{game_stats['player_wins']}"
+  puts "Number of games lost #{game_stats['computer_wins']}"
+  puts "Number of draws #{game_stats['draw']}"
 end
 
-def who_wins(player_play)
-  computer_play = rand(1..5)
-  puts "computer choice #{@choices[computer_play]}"
-  game_result = (@choices.key(player_play) - computer_play) % 5
-  puts "game result is #{game_result}"
+def who_wins(player_play,computer_play,plays,results)
+  game_result = (plays.key(player_play) - computer_play) % 5
   if (game_result.odd?)
-    puts "#{player_play} #{@results[player_play][game_result]} #{@choices[computer_play]}." + " You WIN!!"
-    @games_won += 1
+    puts "#{player_play} #{results[player_play][game_result]} #{plays[computer_play]}." + " You WIN!!"
+    return "player_wins"
   elsif game_result == 0
     puts "It´s a draw"
+    return "draw"
   else
-    game_result = (computer_play - @choices.key(player_play) ) % 5
-    @games_lost += 1
-    puts "#{@choices[computer_play]} #{@results[@choices[computer_play]][game_result]} #{player_play}." + " You loose!!"
+    game_result = (computer_play - plays.key(player_play) ) % 5
+    puts "#{plays[computer_play]} #{results[plays[computer_play]][game_result]} #{player_play}." + " You loose!!"
+    return "computer_wins"
   end
 end
 
-instructions # show instructions
+print_instructions 
 
 # counters
-games_played = 0 
-@games_won = 0
-@games_lost = 0
+game_stats = {"games_played" => 0, "player_wins" => 0, "computer_wins" => 0, "draw" => 0}
 
 # values hashes
-@choices = {1 => "rock", 2 => "paper", 3 => "scissors", 4 => "spock", 5 => "lizard"}
-@results = {"rock" => {1 => "crushes ", 3 => "crushes "}, "paper" => {1 => "covers ", 3 => "disproves "}, "scissors" => {1 => "cuts ", 3 => "decapitates"}, "spock" => {1 => "smashes ", 3 => "vaporizes "}, "lizard" => {1 => "poisons ", 3 => "eats "}}
-error_message = ["upps, wrong option.", "Option is wrong, try again", "That option is not logic, choose again"]
+plays = {1 => "rock", 2 => "paper", 3 => "scissors", 4 => "spock", 5 => "lizard"}
+results = {"rock" => {1 => "crushes ", 3 => "crushes "}, "paper" => {1 => "covers ", 3 => "disproves "}, "scissors" => {1 => "cuts ", 3 => "decapitates"}, "spock" => {1 => "smashes ", 3 => "vaporizes "}, "lizard" => {1 => "poisons ", 3 => "eats "}}
+error_message = ["Oopps, wrong option.", "Option is wrong, try again", "That option is not logic, choose again"]
 
 # Action
 begin
   puts "choose: rock paper scissors lizard spock (exit for ending)".center(50)
   player_play = gets.chomp.downcase
-  games_played += 1 unless player_play == "exit"
+  game_stats["games_played"] += 1 unless player_play == "exit"
   
   if player_play == "exit"
     break
-  elsif @results[player_play].nil? 
+  elsif results[player_play].nil? 
     puts error_message.sample
   else
-    who_wins(player_play)
+    computer_play = rand(1..5)
+    puts "computer choice #{plays[computer_play]}"
+    game_stats[who_wins(player_play,computer_play,plays,results)] += 1
   end
 end while player_play != "exit" 
 
 # results
-total_results(games_played)
+total_results(game_stats)
